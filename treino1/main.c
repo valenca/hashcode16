f768e50f989c0696f599e37b3222effc9eaf6906
Pedro Matias <zydeon@gmail.com>
2016-02-03 22:37:13 +0000

    input beginning

--- treino1/main.c
+++ treino1/main.c
@@ -4,7 +4,20 @@
 #include <limits.h>
 #include <time.h>
 
+#define MAX_R 1000
+#define MAX_S 1000
+#define MAX_P 1000
+#define MAX_U 1000000 // (MAX_R * MAX_S)
+
+
+int R, S, U, P, M;
+
+
 int main(){
+  scanf("%d %d %d %d %d", &R, &S, &U, &P, &M);
+  for(int i; i < u; i++){
+    scanf("%d %d", )
+  }  
 
   return 0;
 }
