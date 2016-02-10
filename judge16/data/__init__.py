964d88221809027eb879631e8dcc7900243b312c
Alexandre Jesus <adbjesus@gmail.com>
2016-02-10 22:06:59 +0000

    Clusters yay

--- judge16/data/__init__.py
+++ judge16/data/__init__.py
@@ -4,7 +4,10 @@ class Slot:
         self.y = y
         self.paint = paint
         self.painted = False
-        self.neigh = None
+        self.neigh = []
+
+    def __str__(self):
+        return "%d %d" % (self.x, self.y)
 
 class Grid:
     def __init__(self, n_rows, n_collumns):
@@ -13,14 +16,23 @@ class Grid:
         self.grid = [[Slot(x,y) for y in range(n_collumns)] for x in range(n_rows)]
         self.paint = []
 
-    def print_grid(self):
+    def print_grid(self, cluster=None):
         for line in self.grid:
             s = ""
             for slot in line:
+                if cluster != None:
+                    a = False
+                    for c in cluster:
+                        if slot == c.center:
+                            s = s + "X"
+                            a = True
+                            break
+                    if a == True:
+                        continue
                 if slot.paint:
-                    s = s + "#"
+                    s = s + "*"
                 else:
-                    s = s + "."
+                    s = s + " "
             print(s)
 
 class Cluster:
@@ -29,23 +41,81 @@ class Cluster:
         self.size = size
         self.grid = Grid(size, size)
 
+    def add_point(self, p):
+        self.grid.paint.append(p)
+
+    def __str__(self):
+        return "Center: %d %d Size: %d" % (self.center.x, self.center.y, self.size)
+
+
+
 def read_input():
     N, M = list(map(int, raw_input().split()))
     g = Grid(N, M)
 
-    for x in range(N):
+    for x in xrange(N):
         line = list(raw_input())
-        for y in range(len(line)):
+        for y in xrange(len(line)):
             if line[y]=="#":
                 g.grid[x][y].paint = True
                 g.paint.append(g.grid[x][y])
 
     return g
 
+def make_clusters(g, d):
+    clusters = []
+
+    # Get neighbors to all points
+    for i in xrange(len(g.paint)):
+        g.paint[i].neigh.append(g.paint[i])
+        for j in xrange(i+1, len(g.paint)):
+            if g.paint[j].x - g.paint[i].x > d:
+                break
+            elif abs(g.paint[j].y - g.paint[i].y) > d:
+                continue
+            else:
+                g.paint[i].neigh.append(g.paint[j])
+                g.paint[j].neigh.append(g.paint[i])
+
+    # Check neigh count
+    sorted_points = sorted(g.paint, reverse=True, key=lambda p:len(p.neigh))
+
+    # Build clusters
+    for p in sorted_points:
+        if len(p.neigh) > 0:
+            c = Cluster(p, d)
+            clusters.append(c)
+            for s in p.neigh[::]:
+                c.add_point(s)
+                if s == p:
+                    continue
+                for sp in s.neigh[::]:
+                    if sp == s:
+                        continue
+                    sp.neigh.remove(s)
+                s.neigh = []
+            p.neigh = []
+            sorted_points.sort(reverse=True, key=lambda p:len(p.neigh))
+        else:
+            break
+
+
+    return clusters
+
+def print_clusters(clusters):
+    for c in clusters:
+        print(c)
+        #for p in c.grid.paint:
+        #    print(p)
 
 if __name__ == '__main__':
     g = read_input()
-    for s in g.paint:
-        print("%d %d" % (s.x, s.y))
-    print(g.paint)
-    g.print_grid()
+    #for s in g.paint:
+    #    print("%d %d" % (s.x, s.y))
+    #print(g.paint)
+  
+    c = make_clusters(g, 1)
+    print_clusters(c)
+    
+    g.print_grid(c)
+
