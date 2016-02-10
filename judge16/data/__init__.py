e6ae043331ba13d1d698255a40eca2fbfc8f6011
Alexandre Jesus <adbjesus@gmail.com>
2016-02-10 20:42:47 +0000

    painted to pain

--- judge16/data/__init__.py
+++ judge16/data/__init__.py
@@ -1,21 +1,22 @@
 class Slot:
-    def __init__(self, x, y, painted=False):
+    def __init__(self, x, y, paint=False):
         self.x = x
         self.y = y
-        self.painted = painted
+        self.paint = paint
+        self.painted = False
 
 class Grid:
     def __init__(self, n_rows, n_collumns):
         self.n_rows = n_rows
         self.n_collumns = n_collumns
         self.grid = [[Slot(x,y) for y in range(n_collumns)] for x in range(n_rows)]
-        self.painted = []
+        self.paint = []
 
     def print_grid(self):
         for line in self.grid:
             s = ""
             for slot in line:
-                if slot.painted:
+                if slot.paint:
                     s = s + "#"
                 else:
                     s = s + "."
@@ -29,14 +30,14 @@ def read_input():
         line = list(raw_input())
         for y in range(len(line)):
             if line[y]=="#":
-                g.grid[x][y].painted = True
-                g.painted.append(g.grid[x][y])
+                g.grid[x][y].paint = True
+                g.paint.append(g.grid[x][y])
 
     return g
 
 if __name__ == '__main__':
     g = read_input()
-    for s in g.painted:
+    for s in g.paint:
         print("%d %d" % (s.x, s.y))
-    print(g.painted)
+    print(g.paint)
     g.print_grid()
