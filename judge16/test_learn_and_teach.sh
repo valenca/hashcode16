b3d5270e940b7cda33b92121ee678c118fc549b2
jsvalenca <jsvalenca@gmail.com>
2016-02-11 17:19:44 +0000

    prequalif

--- judge16/test_learn_and_teach.sh
+++ judge16/test_learn_and_teach.sh
@@ -1,4 +1,11 @@
-for i in {1..800..10}; do
+for i in {1..20..2}; do
+  echo $i
+  pypy main.py $i < tests/learn_and_teach.in > results/learn_and_teach/learn_and_teach_${i}.out
+  echo $(wc -l results/learn_and_teach/learn_and_teach_${i}.out)
+  echo ""
+done
+
+for i in {21..800..10}; do
   echo $i
   pypy main.py $i < tests/learn_and_teach.in > results/learn_and_teach/learn_and_teach_${i}.out
   echo $(wc -l results/learn_and_teach/learn_and_teach_${i}.out)
