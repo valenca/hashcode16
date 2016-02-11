ec7f080f27a986d7c87c9d9397bdb909220545de
Pedro Matias <zydeon@gmail.com>
2016-02-11 19:24:41 +0000

    fiexd timeline

--- qualif16/19/main.py
+++ qualif16/19/main.py
@@ -1,4 +1,18 @@
+import timeline
+import data
 
+class Agent:
+    def __init__(self):
+        self.x = x
+        self.y = y
+
+    def nextAction(self):
+        pass
 
 if __name__ == '__main__':
-    main()
+    T, prods, warehouses, orders, drones = data.read_input()
+
+    q = timeline.Timeline()
+
+    while not q.isEmpty():
+        event = q.nextEvent()
