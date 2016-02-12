5ba303cf5283f4de050995638cd1c5b78361b37a
Alexandre Jesus <adbjesus@gmail.com>
2016-02-12 04:40:12 +0000

    Best version

--- qualif16/alex/data.py
+++ qualif16/alex/data.py
@@ -1,1 +1,113 @@
-../19/data.py
+#!/usr/bin/python2
+
+import math
+
+def distance(x1,y1,x2,y2):
+    return math.ceil( (abs(x1-x2) + abs(y1-y2))**0.5 )
+
+class Drone:
+    def __init__(self, index, x, y, PL):
+        self.index = index
+        self.x = x
+        self.y = y
+        self.max_weight = PL
+        self.items={}
+
+    def load_product(self, prod, size):
+        try:
+            self.items[prod] += size
+        except KeyError:
+            self.items[prod] = size
+
+    def nextAction(self, orders):
+        assert(orders != [])
+
+        next_order = orders[0]
+        best_dist  = dist(next_order.x, next_order.y, self.x, self.y)
+        for o in orders[1:]:
+            d = dist(o.x, o.y, self.x, self.y)
+            if d < best_dist:
+                best_dist = d
+                next_order = o
+
+
+        print len(orders)
+        return 1000000
+
+
+class Warehouse:
+    def __init__(self, index, x, y, items):
+        self.index = index
+        self.x = x
+        self.y = y
+        self.items = items
+
+class Order:
+
+    def __init__(self, x, y, index):
+        self.x = x
+        self.y = y
+        self.index = index
+        self.items={}
+
+
+    def total_weight(self):
+        t = 0
+        for it in self.items:
+            t += (it.weight * self.items[it])
+        return t
+
+    def n_items(self):
+        t = 0
+        for it in self.items:
+            t += self.items[it]
+        return t
+
+    def dif_items(self):
+        return len(self.items.keys())
+
+    def add_product(self, prod, size=1):
+        try:
+            self.items[prod] += size
+        except KeyError:
+            self.items[prod] = size
+
+class Product:
+    def __init__(self, index, weight):
+        self.index = index
+        self.weight = weight
+	
+def read_input():
+    n_rows,n_cols,D,T,PL = map(int,raw_input().split())
+    
+    P = input()
+    i = raw_input().split()
+    prods = []
+    for j in range(len(i)):
+        prods.append(Product(j,int(i[j])))
+    
+    W = input()
+    warehouses = []
+    i = 0
+    for _ in range(W):
+        x,y = map(int,raw_input().split())
+        items = map(int,raw_input().split())
+        warehouses.append(Warehouse(i,x,y,items))
+        i += 1
+
+    C = input()
+    orders = []
+    for i in range(C):
+        x, y = map(int,raw_input().split())
+        orders.append(Order(x,y,i))
+        n_prods = input()
+        for j in map(int,raw_input().split()):
+            orders[-1].add_product(prods[j])
+
+    drones = [Drone(i, warehouses[0].x, warehouses[0].y, PL) for i in range(D)]
+
+    return T, prods, warehouses, orders, drones, n_rows, n_cols
+
+
+if __name__ == "__main__":
+    read_input()
--- qualif16/alex/main.py
+++ qualif16/alex/main.py
@@ -1,11 +1,43 @@
 import data
 from math import sqrt, ceil
 
+hilbert_map = {
+   'a': {(0, 0): (0, 'd'), (0, 1): (1, 'a'), (1, 0): (3, 'b'), (1, 1): (2, 'a')},
+   'b': {(0, 0): (2, 'b'), (0, 1): (1, 'b'), (1, 0): (3, 'a'), (1, 1): (0, 'c')},
+   'c': {(0, 0): (2, 'c'), (0, 1): (3, 'd'), (1, 0): (1, 'c'), (1, 1): (0, 'b')},
+   'd': {(0, 0): (0, 'a'), (0, 1): (3, 'c'), (1, 0): (1, 'd'), (1, 1): (2, 'd')},
+}
+
+def point_to_hilbert(x, y, h, w):
+    order=1
+    while order<max(h,w):
+        order*=2
+    current_square = 'a'
+    position = 0
+    for i in range(order - 1, -1, -1):
+        position <<= 2
+        quad_x = 1 if x & (1 << i) else 0
+        quad_y = 1 if y & (1 << i) else 0
+        quad_position, current_square = hilbert_map[current_square][(quad_x, quad_y)]
+        position |= quad_position
+    return position
+
+
 def dist(x1, y1, x2, y2):
     return ceil(sqrt((x1-x2)*(x1-x2) + (y1-y2)*(y1-y2)))
 
+def dist_warehouses(o, warehouses):
+    m = dist(warehouses[0].x, warehouses[0].y, o.x, o.y)
+    for w in warehouses:
+        if dist(w.x, w.y, o.x, o.y) < m:
+            m = dist(w.x, w.y, o.x, o.y)
+    return m
+        
 
-T, prods, warehouses, orders, drones = data.read_input()
+T, prods, warehouses, orders, drones, nr, nc = data.read_input()
+orders2 = orders[::]
+
+orders.sort(key=lambda o: o.total_weight())
 
 # Sort warehouses per order
 ow = []
@@ -14,7 +46,6 @@ for o in orders:
     for w in warehouses:
         ow[-1].append([w, dist(o.x, o.y, w.x, w.y)])
     ow[-1].sort(key=lambda w: w[1])
-
 
 work_list = []
 # Get items from warehouses
@@ -25,7 +56,7 @@ for i in range(len(orders)):
                 if w[0].items[it.index] > 0:
                     orders[i].items[it] -= 1
                     w[0].items[it.index] -= 1
-                    work_list.append([w[0], i, it])
+                    work_list.append([w[0], orders[i].index, it])
                     break
 
 # Remove unfinished orders of work list
@@ -43,6 +74,55 @@ for i in range(len(work_list)):
 for it in to_remove[::-1]:
     work_list.pop(it)
 
+ot = [0] * len(orders)
+tasks = []
+d = 0
+w = 0
+d_task = {}
+# Give tasks to drones
+for i in range(len(work_list)):
+    if work_list[i][1] == work_list[i-1][1]:
+        if w + work_list[i][2].weight > drones[d].max_weight:
+            # print loads
+            for t in d_task:
+                tasks.append("%d L %d %d %d" % (d, int(t.split(":")[0]), int(t.split(":")[1]), d_task[t]))
+            for t in d_task:
+                tasks.append("%d D %d %d %d" % (d, work_list[i][1], int(t.split(":")[1]), d_task[t]))
+            w = 0
+            d_task = {}
+
+    elif work_list[i][1] != work_list[i-1][1]:
+        for t in d_task:
+            tasks.append("%d L %d %d %d" % (d, int(t.split(":")[0]), int(t.split(":")[1]), d_task[t]))
+        for t in d_task:
+            tasks.append("%d D %d %d %d" % (d, work_list[i-1][1], int(t.split(":")[1]), d_task[t]))
+
+        d = (d + 1) % len(drones)
+        w = 0
+        d_task = {}
+        
+    try:
+        d_task["%d:%d" % (work_list[i][0].index, work_list[i][2].index)] += 1
+        ot[work_list[i][1]] += 1 + dist(work_list[i][0].x, work_list[i][0].y, orders2[work_list[i][1]].x, orders2[work_list[i][1]].y) 
+    except KeyError:
+        d_task["%d:%d" % (work_list[i][0].index, work_list[i][2].index)] = 1
+        ot[work_list[i][1]] += 1 + dist(work_list[i][0].x, work_list[i][0].y, orders2[work_list[i][1]].x, orders2[work_list[i][1]].y) 
+
+    w += work_list[i][2].weight
+
+
+for t in d_task:
+    tasks.append("%d L %d %d %d" % (d, int(t.split(":")[0]), int(t.split(":")[1]), d_task[t]))
+for t in d_task:
+    tasks.append("%d D %d %d %d" % (d, work_list[-1][1], int(t.split(":")[1]), d_task[t]))
+
+
+
+
+# AGAIN
+work_list.sort(key=lambda wl: (ot[wl[1]],ot[wl[0].index]))
+
+ot = [0] * len(orders)
 tasks = []
 d = 0
 w = 0
@@ -73,6 +153,7 @@ for i in range(len(work_list)):
         d_task["%d:%d" % (work_list[i][0].index, work_list[i][2].index)] += 1
     except KeyError:
         d_task["%d:%d" % (work_list[i][0].index, work_list[i][2].index)] = 1
+
     w += work_list[i][2].weight
 
 
